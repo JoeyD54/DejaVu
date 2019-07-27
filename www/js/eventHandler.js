e597fd4b24220a565bae322dcfb27d791bc8fa29
Joey Domino <playdude224@hotmail.com>
2019-07-27 19:23:48 -0400

    murp
    bb

--- www/js/eventHandler.js
+++ www/js/eventHandler.js
@@ -3,19 +3,24 @@
 * for the program                                 * 
 **************************************************/
 
+//document.getElementById('doXMLWork').addEventListener('click', loadXMLTable);
+//document.addEventListener('deviceready', loadXMLTable());
+
 document.getElementById('insertButton').addEventListener('click', showModal, true);
 document.addEventListener('backbutton', onBackKeyDown, false);
 document.getElementById('closeScreen').addEventListener('click', closeModal);
 document.getElementById('cancelButton').addEventListener('click', closeModal);
-document.getElementById('submitButton').addEventListener('click', loadXMLTable);
+document.getElementById('submitButton').addEventListener('click', addToXML);
+document.addEventListener('deviceready', loadXMLTable());
 
 function onLoad(){
-    document.addEventListener('deviceready', showXMLTable, false);
+    document.addEventListener('deviceready', loadXMLTable, false);
 
 }
 
 function onDeviceReady(){
     //load up the XML table.
+    //loadXMLTable();
 }
 
 function showModal(){
@@ -33,8 +38,25 @@ function onBackKeyDown(event){
 }
 
 function loadXMLTable(){
-    document.getElementById('printHere').innerHTML = "Before XML call";
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            displayXMLTable(this);
+        }
+    };
+    //xmlhttp.open("GET", "xml/toDoList.xml", true);
+    xmlhttp.open("GET", "xml/toDoList.xml", true);
+    xmlhttp.send();
+    /*
     var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "toDoList.xml", true);
+    xhttp.send();
+    displayXMLTable(xhttp);
+    */
+    /*
+    var xhttp = new XMLHttpRequest();
+    document.getElementById('printHere').innerHTML = xhttp;
+    
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             displayXMLTable(this);
@@ -42,14 +64,34 @@ function loadXMLTable(){
     };
     xhttp.open("GET", "xml/toDoList.xml", true);
     xhttp.send();
+    */
 }
 
 function displayXMLTable(xml){
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table="<tr><th>Item</th><th>Destination</th></tr>";
+    var x = xmlDoc.getElementsByTagName("todo");
+    for (i = 0; i <x.length; i++) { 
+        table += "<tr><td>" +
+        x[i].getElementsByTagName("item")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("dest")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        "<span id=i onclick='removeXMLDocItem()' class='close'>&times;</span>" +
+        "</td></tr>";
+      
+    }
+    document.getElementById("printTable").innerHTML = table;
+    /*
+    document.getElementById('printHere').innerHTML = xml;
     var x, i, list, item, dest, xmlDoc;
     xmlDoc = xml.responseXML;
     item = "";
     dest = "";
     x = xmlDoc.getElementsByTagName('todo');
+    var y = x[0].getElementsByTagName('item')[0].childNodes[0].nodeValue;
+    document.getElementById('printHere').innerHTML = y;
     for(i = 0; i < x.length; i++){
 
         item = x[i].getElementsByTagName('item')[0].childNodes[0].nodeValue;
@@ -61,11 +103,10 @@ function displayXMLTable(xml){
             "<div class='w3-bar-item'> " + 
                 "<span class='w3-large'>" x[i].getElementsByTagName('item')[0].childNodes[0].nodeValue; "</span>" +
                 "<"
-            "</div";
-            */            
+            "</div";            
     }
-   document.getElementById('test').innerHTML = 'success';
-
+   document.getElementById('printHere').innerHTML = item;
+*/
 }
 
 /*
